7f665bcd6f569a6804e4820d9c2052220725660c
キャベツウニ <akihitoukami@gmail.com>
2022-01-14 11:07:12 +0900

    1

---
+++ Src_Culture/ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Globalization;
+using System.Threading;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //const string = "ja-JP";
+            const string cultureName = "ru-RU";
+
+            //CultureInfo.CurrentCulture = new CultureInfo(cultureName, false);
+            //CultureInfo.CurrentUICulture = new CultureInfo(cultureName, false);
+            CultureInfo.DefaultThreadCurrentCulture = new CultureInfo(cultureName, false);
+            CultureInfo.DefaultThreadCurrentUICulture = new CultureInfo(cultureName, false);
+            //Thread.CurrentThread.CurrentCulture = new CultureInfo(cultureName, false);
+            //Thread.CurrentThread.CurrentUICulture = new CultureInfo(cultureName, false);
+
+            Console.WriteLine("CultureInfo.CurrentCulture.Name : " + CultureInfo.CurrentCulture.Name);
+            Console.WriteLine("CultureInfo.CurrentUICulture.Name : " + CultureInfo.CurrentUICulture.Name);
+            Console.WriteLine("CultureInfo.DefaultThreadCurrentCulture.Name : " + CultureInfo.DefaultThreadCurrentCulture.Name);
+            Console.WriteLine("CultureInfo.DefaultThreadCurrentUICulture.Name : " + CultureInfo.DefaultThreadCurrentUICulture.Name);
+            Console.WriteLine("Thread.CurrentThread.CurrentCulture.Name : " + Thread.CurrentThread.CurrentCulture.Name);
+            Console.WriteLine("Thread.CurrentThread.CurrentUICulture.Name : " + Thread.CurrentThread.CurrentUICulture.Name);
+        }
+    }
+}
